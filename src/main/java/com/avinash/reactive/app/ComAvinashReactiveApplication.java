b742610b43dc15668cc60613b8cee32a8755a974
Avinash Tiwari <qwe123kids@gmail.com>
2018-01-14 16:41:34 -0700

    Spring Reactive Hello world

---
+++ src/main/java/com/avinash/reactive/app/ComAvinashReactiveApplication.java
@@ -0,0 +1,12 @@
+package com.avinash.reactive.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComAvinashReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComAvinashReactiveApplication.class, args);
+	}
+}
